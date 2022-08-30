a9e80a61f9e235de314b5602305683b321a1b6c3
Mentol <admental1@gmail.com>
2022-08-30 17:45:11 +0200

    Initial Commit

---
+++ Shared/gps_speedometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  gps_speedometerApp.swift
+//  Shared
+//
+//  Created by Mentol on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct gps_speedometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
